1553b5403d7904adf53a70c10e29bcd6271f33e1
e3stadnicar <e3stadnicar@gmail.com>
2016-06-05 14:59:12 -0400

    fulfilled requirment for $12/hr before bedtime pay.corrected start and stop time exception

--- src/main/java/com/mycompany/java_katas/BabySitter.java
+++ src/main/java/com/mycompany/java_katas/BabySitter.java
@@ -10,15 +10,18 @@ import java.util.Properties;
  * leaves no later than 4:00AM
  * gets paid $12/hour from start-time to bedtime
  * gets paid $8/hour from bedtime to midnight
- * gets paid $16/hour from midnight to end of job
+ * gets paid $16/hour from midnight to end of job 
  * gets paid for full hours (no fractional hours)
  * @author e3stadnicar
  */
 public class BabySitter {
 	
-	int startAllowed;
-	int stopAllowed;
-	int startTime;
+	private int startAllowed;
+	private int stopAllowed;
+	private int startTime;
+	private int stopTime;
+	private int bedTime;
+	private int beforeBedPay;
 	
 	/*
 	 * Babysitter constructor
@@ -35,6 +38,7 @@ public class BabySitter {
 			// Initialize start time allowed
 			this.startAllowed = Integer.parseInt(props.getProperty("START_ALLOWED"));
 			this.stopAllowed = Integer.parseInt(props.getProperty("STOP_ALLOWED"));
+			this.beforeBedPay = Integer.parseInt(props.getProperty("BEFORE_BED_PAY"));
 			
 		} catch (IOException ex) {
 			ex.printStackTrace();
@@ -53,9 +57,16 @@ public class BabySitter {
 	 * Given a start, stop, and bed time calculates expected babysitter pay
 	 * @param start The babysitter start time (Using integer for this exercise)
 	 */
-	public void calculate(int start, int stop) throws BabySitterException {
+	public int calculate(int start, int stop, int bedTime) throws BabySitterException {
+		//TODO: Do not allow start before stop
+				
 		setStartTime(start);
 		setStopTime(stop);
+		setBedTime(bedTime);
+		
+		int payment = this.beforeBedPay * (this.bedTime - this.startTime);
+		
+		return payment;
 	}
 	
 	/*
@@ -63,8 +74,8 @@ public class BabySitter {
 	 * @param start Babysitter start time
 	 */
 	private void setStartTime(int start) throws BabySitterException {
-		if (start < this.startAllowed) {
-			throw new BabySitterException("Babysitter start time cannot be before 5 PM.");
+		if (start < this.startAllowed && start > this.stopAllowed) {
+			throw new BabySitterException("Babysitter start time cannot be before " + this.startAllowed + " PM or after " + this.stopAllowed + " AM.");
 		}
 		this.startTime = start;
 	}
@@ -74,9 +85,21 @@ public class BabySitter {
 	 * @param stop Babysitter stop time
 	 */
 	private void setStopTime(int stop) throws BabySitterException {
-		if (stop > this.stopAllowed) {
-			throw new BabySitterException("Babysitter stop time cannot be after 4 AM.");
+		if (stop > this.stopAllowed && stop < this.startAllowed) {
+			throw new BabySitterException("Babysitter stop time cannot be before " + this.startAllowed + " PM or after " + this.stopAllowed + " AM.");
 		}
+		this.stopTime = stop;
+	}
+	
+	/*
+	 * Sets Babysitter bed time
+	 * @param Baby or child's bed time
+	 */
+	private void setBedTime(int toBed) throws BabySitterException {
+		if ((toBed < startTime )) {
+			throw new BabySitterException("Bed time must be within sitting hours.");
+		}
+		this.bedTime = toBed;
 	}
 
 	/**
--- src/test/java/com/mycompany/java_katas/BabySitterTest.java
+++ src/test/java/com/mycompany/java_katas/BabySitterTest.java
@@ -1,5 +1,8 @@
 package com.mycompany.java_katas;
 
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,7 +22,7 @@ public class BabySitterTest {
 	 */
 	@Test(expected=BabySitterException.class)
 	public void testCannotStartBeforeFivePM() throws BabySitterException {
-		babysitter.calculate(16, 4);
+		babysitter.calculate(16, 4, 4);
 	}
 	
 	/*
@@ -27,11 +30,16 @@ public class BabySitterTest {
 	 */
 	@Test(expected=BabySitterException.class)
 	public void testCannotStopAfterFourAM() throws BabySitterException {
-		babysitter.calculate(17, 5);
+		babysitter.calculate(17, 5 , 4);
 	}
 	
-	// gets paid $12/hour from start-time to bedtime
-	
+	/*
+	 * Test calculate babysitter payment of $12/hour form start-time to bedtime
+	 */
+	@Test
+	public void testCalculatePaymentFromStartToBedTime() throws BabySitterException {
+		assertEquals(48, babysitter.calculate(17, 21, 21));
+	}
 	
 	// gets paid $8/hour from bedtime to midnight
 	
